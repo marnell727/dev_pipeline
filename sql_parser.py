6757a90ad2ea6026af043ddd062326476de57554
Michael Arnell <michael.olson.arnell@gmail.com>
2021-11-14 19:53:23 -0700

    Initial version sql_parser

--- sql_parser.py
+++ sql_parser.py
@@ -1,43 +1,35 @@
-query = '''SELECT name, make, model, price FROM Products  WHERE price = 49.99
-ORDER BY price DESC
-LIMIT 5'''
+query = '''SELECT name, make, model, price FROM Products WHERE price = 49.99 ORDER BY price DESC LIMIT 5'''
 
 def parse_sql(query):
-    sql_dict = {}
-    if 'SELECT ' in query:
-        if ' FROM' in query:
-            select_cl = query.partition('SELECT ')[2].partition(' FROM')[0]
-        else:
-            select_cl = query.partition('SELECT ')[2]
-    if 'FROM ' in query:
-        if ' WHERE' in query:
-            from_cl = query.partition('FROM ')[2].partition(' WHERE')[0]
-    if 'WHERE ' in query:
-        if ' ORDER BY' in query:
-            where_cl = query.partition('WHERE ')[2].partition(' ORDER BY')[0]
-    if 'ORDER BY ' in query:
-        if ' LIMIT' in query:
-            order_cl = query.partition('ORDER BY ')[2].partition(' LIMIT')[0]
-    if 'LIMIT ' in query:
-            limit_cl = query.partition('ORDER BY ')[2]
-
+   sql_dict = {}
+   clauses = {}
+   keywords = ['SELECT','FROM','WHERE','ORDER BY','LIMIT']
+   subquery = query
+   for i in range(len(keywords) - 1):
+      if keywords[i] in subquery:
+         subquery = subquery.partition(keywords[i] + ' ')[2]
+         for k in range(1,len(keywords) - 1):
+            if keywords[i+k] in subquery:
+               clauses[keywords[i]] = subquery.partition(' ' + keywords[i+k])[0]
+               break
+         if not clauses[keywords[i]]:
+            clauses[keywords[i]] = subquery
+   if keywords[len(keywords) - 1] in subquery:
+      clauses[keywords[len(keywords) - 1]] = subquery.partition(keywords[len(keywords) - 1] + ' ')[2]
+   print(clauses)
+   if clauses['SELECT']:
+      sql_dict['fields'] = clauses['SELECT'].split(', ')
+   if clauses['FROM']:
+      sql_dict['table'] = clauses['FROM']
+   if clauses['WHERE']:
+      sql_dict['where'] = {}
+      sql_dict['where'][clauses['WHERE'].split(' = ')[0]] = clauses['WHERE'].split(' = ')[0]
+   if clauses['ORDER BY']:
+      sql_dict['order_by'] = {}
+      sql_dict['order_by']['field'] = clauses['ORDER BY'].split(' ')[0]
+      sql_dict['order_by']['order'] = clauses['ORDER BY'].split(' ')[1]
+   if clauses['LIMIT']:
+      sql_dict['limit'] = clauses['LIMIT']
+   return sql_dict
 
 print(parse_sql(query))
-
-{
-   'fields': [
-      'name', 
-      'make', 
-      'model', 
-      'price'
-   ],
-   'table': 'Products',
-   'where': {
-      'price': 49.99
-   },
-   'order_by': {
-      'field': 'price',
-      'order': 'DESC'
-   },
-   'limit': 5
-}
